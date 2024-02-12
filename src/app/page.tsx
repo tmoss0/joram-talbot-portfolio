102fd166c946f3211c10821f4297a3f0c7f84c2e
Tim Moss <tmoss726@hotmail.com>
2024-02-12 15:38:36 -0500

    Remove outdated font class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
-import localFont from 'next/font/local';
-const beauxFont = localFont({ src: '../../public/fonts/BEAUX-Display.ttf' });
-
 export default function Home() {
   return (
     <div className='my-16'>
-      <h1
-        className={`${beauxFont.className} text-[200px] uppercase leading-none`}
-      >
-        DESIGN
-      </h1>
-      <h1
-        className={`${beauxFont.className} text-[200px] uppercase leading-none`}
-      >
-        MOTION
-      </h1>
-      <h1
-        className={`${beauxFont.className} text-[200px] uppercase leading-none`}
-      >
-        OTHER
-      </h1>
+      <h1 className={`text-[200px] uppercase leading-none`}>DESIGN</h1>
+      <h1 className={` text-[200px] uppercase leading-none`}>MOTION</h1>
+      <h1 className={`text-[200px] uppercase leading-none`}>OTHER</h1>
     </div>
   );
 }
