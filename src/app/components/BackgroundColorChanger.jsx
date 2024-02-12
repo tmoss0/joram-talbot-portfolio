3a3125e73a27c493c8566eac725ab6817f7430f1
Tim Moss <tmoss726@hotmail.com>
2024-02-12 16:20:52 -0500

    Remove comments

--- src/app/components/BackgroundColorChanger.jsx
+++ src/app/components/BackgroundColorChanger.jsx
@@ -1,6 +1,4 @@
-// components/BackgroundColorChanger.js
 'use client';
-
 import { useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 
@@ -8,24 +6,17 @@ export default function BackgroundColorChanger() {
   const pathname = usePathname();
 
   useEffect(() => {
-    // Define your background colors for each path
     const colors = {
       '/': '#1a6843',
       '/motion': '#1a6843',
-      '/print': '#C4BFFF',
+      '/print': '#c4bfff ',
       '/digital': '#ffd24d',
       '/contact': '#1a6843',
     };
 
-    // Set the background color based on the current pathname
-    const color = colors[pathname];
+    const color = colors[pathname] || colors['/'];
     if (color) {
       document.documentElement.style.backgroundColor = color;
-    } else {
-      // Default color if the pathname isn't defined in the colors object
-      document.documentElement.style.backgroundColor = '#ffffff';
     }
   }, [pathname]);
-
-  return null; // No need to render anything
 }
